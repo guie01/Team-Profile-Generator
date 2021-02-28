4f5daa105777e896e649d5379683e3753bc703c1
Guillermo Garcia <ggq0107@hotmail.com>
2021-02-27 19:54:33 -0500

    Employee.test passes all test successfully

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,5 +1,6 @@
 const Employee = require("../lib/Employee");
 
+//OBJECT TEST 
 describe("Employee", () => {
     it("Create a new object", () =>{
         const e = new Employee();
@@ -25,4 +26,36 @@ describe("Employee", () => {
         const e = new Employee("Guillermo", "12345", testvalue);
         expect(e.email).toBe(testvalue);
     });
+});
+
+describe("getName()", () =>{
+    it("The funciton is able to retrieve the name from the object", ()=>{
+        const testvalue = "Guillermo";
+        const e = new Employee(testvalue);
+        expect(e.getName()).toBe(testvalue);
+    })
+});
+
+describe("getId()", () =>{
+    it("The funciton is able to retrieve the ID from the object", ()=>{
+        const testvalue = "12345";
+        const e = new Employee("Guillermo",testvalue);
+        expect(e.getId()).toBe(testvalue);
+    })
+})
+
+describe("getEmail()", () =>{
+    it("The funciton is able to retrieve the email from the object", ()=>{
+        const testvalue = "test@gmail.com";
+        const e = new Employee("Guillermo","12345", testvalue);
+        expect(e.getEmail()).toBe(testvalue);
+    })
+})
+
+describe("getRole()", () =>{
+    it("The funciton is able to retrieve the role from the object", ()=>{
+        const testvalue = "Employee";
+        const e = new Employee("Guillermo","12345", "test@gmail.com");
+        expect(e.getRole()).toBe(testvalue);
+    })
 })

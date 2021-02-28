58ae30ae4eafb0421cf220c0d79817221f97bb4a
Guillermo Garcia <ggq0107@hotmail.com>
2021-02-27 19:37:35 -0500

    added object test, all pass

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -4,5 +4,25 @@ describe("Employee", () => {
     it("Create a new object", () =>{
         const e = new Employee();
         expect(typeof(e)).toBe("object");
-    })
+    });
+    it("Able to add a name to the object", () =>{
+        const name = "Guillermo";
+        const e = new Employee(name);
+        expect(e.name).toBe(name);
+    });
+    it("Able to add a name to the object", () =>{
+        const name = "Guillermo";
+        const e = new Employee(name);
+        expect(e.name).toBe(name);
+    });
+    it("Able to add a id to the object", () =>{
+        const testvalue = "12345";
+        const e = new Employee("Guillermo", testvalue);
+        expect(e.id).toBe(testvalue);
+    });
+    it("Able to add a email to the object", () =>{
+        const testvalue = "test@gmail.com";
+        const e = new Employee("Guillermo", "12345", testvalue);
+        expect(e.email).toBe(testvalue);
+    });
 })
